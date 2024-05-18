7d3eb969491fec78a6d6333c3fa0af96933ee380
DawidKopecki <davidisheree@hotmail.com>
2024-05-18 17:11:31 +0100

    COMMMIT

---
+++ routes/manga.routes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const mangaExpressRoute = express.Router();
+let MangaSchema = require('../model/manga.model');
+
+// Search route
+mangaExpressRoute.route('/search/:key').get(async (req, res) => {
+    try {
+        let data = await MangaSchema.find({
+            "$or": [
+                { title: { $regex: req.params.key, $options: 'i' } },
+                { author: { $regex: req.params.key, $options: 'i' } },
+                { year_written: { $regex: req.params.key } },
+                { NumberOfVolumes: { $regex: req.params.key } }
+            ]
+        });
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+mangaExpressRoute.route('/').get((req, res, next) => {
+    MangaSchema.find((error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+mangaExpressRoute.route('/manga/:id').get((req, res, next) => {
+    MangaSchema.findById(req.params.id, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+mangaExpressRoute.route('/add-manga').post((req, res, next) => {
+    MangaSchema.create(req.body, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+mangaExpressRoute.route('/delete-manga/:id').delete((req, res, next) => {
+    MangaSchema.findByIdAndRemove(req.params.id, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                msg: data
+            });
+        }
+    });
+});
+
+mangaExpressRoute.route('/update-manga/:id').put((req, res, next) => {
+    MangaSchema.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+    }, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data);
+            console.log('Manga Updated successfully!');
+        }
+    });
+});
+
+
+
+module.exports = mangaExpressRoute;

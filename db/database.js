549fa209968eda2014cc28c36c2e43a4d59f9f9e
DawidKopecki <davidisheree@hotmail.com>
2024-05-19 17:54:11 +0100

    commit

--- db/database.js
+++ db/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    db: 'mongodb+srv://s00212387:RNIMhrjVHhTLZFWX@manga.vvonhnm.mongodb.net/'
+    db: 'mongodb+srv://JohnSmith:ZcrvOBrYzh0C06NW@cluster0.nrkdntd.mongodb.net/manga'
 }
